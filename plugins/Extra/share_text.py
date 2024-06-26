471abf9453484007dfe72e9abe48ce36f9bc19eb
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-06-26 10:33:16 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -14,16 +14,16 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["share_text", "share", "sharetext"]))
 async def share_text(client, message):
-    vj = await client.ask(chat_id = message.from_user.id, text = "Now Send me your text.")
-    if vj and (vj.text or vj.caption):
-        input_text = vj.text or vj.caption
+    black  = await client.ask(chat_id = message.from_user.id, text = "Now Send me your text.")
+    if black and (black.text or black.caption):
+        input_text = black.text or black.caption
     else:
-        await vj.reply_text(
+        await black.reply_text(
             text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/vj_botz")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Main Channel", url=f"https://t.me/TheBlackXYZ")]])
             )                                                   
         return
-    await vj.reply_text(
+    await black.reply_text(
         text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
     )
